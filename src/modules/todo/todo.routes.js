37f36f4ad9db7ec1ae2ab1bf2eed2253cdeb6a53
Soham Mali <malishoham0007@gmail.com>
2026-06-03 15:05:12 +0530

    Add Todo routes with JWT verification and CRUD endpoints

---
+++ src/modules/todo/todo.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import verifyJWT from "../../middleware/auth.middleware.js";
+
+import {
+  createTodoController,
+  getSingleTodoController,
+  updateTodoController,
+  deleteTodoController,
+} from "./todo.controller.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.post("/", createTodoController);
+
+router.get("/:todoId", getSingleTodoController);
+
+router.patch("/:todoId", updateTodoController);
+
+router.delete("/:todoId", deleteTodoController);
+
+export default router;
