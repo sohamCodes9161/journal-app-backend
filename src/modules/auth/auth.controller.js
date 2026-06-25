757b36f0654fabf6b30e864402961da8e59e8fcf
Soham Mali <malishoham0007@gmail.com>
2026-06-25 21:46:16 +0530

    Update cookie SameSite attribute to 'none' for login, logout, and refresh token responses

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -26,7 +26,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: false,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -52,7 +52,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: false,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -80,6 +80,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: false,
+    sameSite: "none",
   };
 
   return res
