2ab049f4ffe8e6a506657792cb27b86e11916cbe
Soham Mali <malishoham0007@gmail.com>
2026-06-25 19:53:14 +0530

    Update CORS configuration to allow multiple origins

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://journal-app-frontend-dusky.vercel.app/",
+    ],
     credentials: true,
   })
 );
