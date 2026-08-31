0d7b0486dafb1764d02e6bc90a34b8cff33f4f86
Soham Mali <malishoham0007@gmail.com>
2026-09-01 01:04:18 +0530

    Fix: Set maxAge for refreshAccessToken cookie options to 7 days

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -82,6 +82,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
   return res
