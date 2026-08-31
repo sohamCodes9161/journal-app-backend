142fcafa29b40a6a671ce9da5e4d9f8406eb2f62
Soham Mali <malishoham0007@gmail.com>
2026-09-01 00:57:32 +0530

    Fix: Update sameSite attribute to "none" for cookie options in login, logout, and refreshAccessToken functions

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -25,7 +25,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -52,7 +52,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -81,7 +81,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
   };
 
   return res
