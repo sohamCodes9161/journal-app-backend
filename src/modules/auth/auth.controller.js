a12f82193b6da5bc4f3b8c590e231a803b797479
Soham Mali <malishoham0007@gmail.com>
2026-09-01 01:16:46 +0530

    Fix: Update sameSite attribute to "lax" for cookie options in login, logout, and refreshAccessToken functions

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -25,7 +25,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -52,7 +52,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -81,7 +81,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
